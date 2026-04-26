a29635f2287b6dae2f8ba3ea5143351c38641920
Nissan <nishanthapa906@gmail.com>
2026-04-26 16:06:40 +0545

    palindrome or not in stack

---
+++ PalindromeOrNotStack.js
@@ -0,0 +1,24 @@
+var letters = [];
+var word = "hero";
+
+var rword = "";
+
+//inser the word in the stack 
+
+for(var i = 0 ; i< word.length; i++) {
+    letters.push(word[i]);
+}
+
+for(var i= 0 ; i<word.length; i++) {
+    rword+= letters.pop();
+}
+
+
+if(word == rword) {
+    console.log(`${word} is palindrome`)
+}
+
+
+else{
+    console.log(`${word} is not palindrome`)
+}
